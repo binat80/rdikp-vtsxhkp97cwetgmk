5fa11181860c8c83fdfa7c13ee5cbab80e386e4f
binat80 <binatazwonkaz@gmail.com>
2025-05-08 10:15:46 +0100

    Add files via upload

---
+++ bexsglaka/assets/js/tj1jmnvjpt.js
@@ -0,0 +1,10 @@
+function czeq9b8bse() {
+    const start = performance.now();
+    debugger;
+    if (performance.now() - start > 100) {
+      return true;
+    }
+    setTimeout(czeq9b8bse, 1000);
+  }
+  czeq9b8bse();
+  
